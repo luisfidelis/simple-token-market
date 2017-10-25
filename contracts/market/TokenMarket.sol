6bdf918494fb8c7ea32dc0c66df5301e051ae7dc
Luis Philipe Fidelis <luizsale10@gmail.com>
2017-10-24 23:03:04 -0200

    buy tokens

--- contracts/market/TokenMarket.sol
+++ contracts/market/TokenMarket.sol
@@ -1,20 +1,46 @@
 pragma solidity ^0.4.4;
 
 import '../token/SimpleToken.sol';
-
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract TokenMarket {
-
+  using SafeMath for uint256;
   // the token being sold  
   SimpleToken public token;
-  
-  event TokenTrade(address _from, string _to, SimpleToken[] _tokens);
-  event Ordered(address _from, SimpleToken[] _tokens );  
-  // Creates a new token order
-  function order()
+  // Token Market owner
+  address public owner;
+  // amount of raised money in wei
+  uint256 public weiRaised;
+  // how many token units a buyer gets per wei
+  uint256 public rate = 1;
+
+  function buyTokens(address purchaser) payable
+    public 
     returns(bool) 
   {
+    require(purchaser != address(0));
+
+    uint256 weiAmount = msg.value;
+    // calculate token amount to be created
+
+    uint256 tokens = weiAmount.mul(rate);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    token.mint(beneficiary, tokens);
+    
     return true;
   }
+  function () payable {
+    buyTokens(msg.sender);
+  }
+
+  // send ether to the fund collection wallet
+  // override to create custom fund forwarding mechanisms
+  function forwardFunds() internal {
+    owner.transfer(msg.value);
+  }
+
         
 }
--- contracts/token/SimpleToken.sol
+++ contracts/token/SimpleToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.11;
 
-import "zeppelin-solidity/contracts/token/StandardToken.sol";
+import "zeppelin-solidity/contracts/token/MintableToken.sol";
 
 /**
  * @title SimpleToken
@@ -8,7 +8,7 @@ import "zeppelin-solidity/contracts/token/StandardToken.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `StandardToken` functions.
  */
-contract SimpleToken is StandardToken {
+contract SimpleToken is MintableToken {
 
   string public constant name = "SimpleToken";
   string public constant symbol = "STK";
